140d28101ffea938b09506faed9fcb4abb59e67d
AmirHossein Abdolmotallebi <amir.a1376@gmail.com>
2023-03-01 06:26:41 +0330

    add publication for compose integration

--- plugins/watcher/compose/build.gradle.kts
+++ plugins/watcher/compose/build.gradle.kts
@@ -1,6 +1,20 @@
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
+    `maven-publish`
+}
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+publishing {
+    publications {
+        create("maven", MavenPublication::class) {
+            afterEvaluate {
+                from(components["java"])
+            }
+        }
+    }
 }
 dependencies {
     implementation(project(":core"))
